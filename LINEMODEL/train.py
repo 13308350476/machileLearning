622f4fe9daac539ad900983cd5985ad2d6f1b71c
13308350476 <zxc723067485811@qq.com>
2023-06-06 18:23:15 +0800

    2023 06 06

--- LINEMODEL/train.py
+++ LINEMODEL/train.py
@@ -1,39 +1,32 @@
 import torch
 import numpy as np
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 
-row_data = np.loadtxt('/datasets/diabetes/diabetes.csv.gz', delimiter=',', dtype=np.float32)
-x_data = torch.from_numpy(row_data[:-7, :-1])
-y_data = torch.from_numpy(row_data[:-7, -1:])
+class DiateatsDatasets(Dataset):
+    def __init__(self, filepath):
+        xy = np.loadtxt(filepath, delimter=',', dtype=np.float32)
+        self.x_data = torch.from_numpy(xy[:, :-1])
+        self.y_data = torch.from_numpy(xy[:, [-1]])
+        self.len = xy.shape[0]
 
-class MYMODEL(torch.nn.Module):
-    def __init__(self):
-        super(MYMODEL, self).__init__()
-        self.linear1 = torch.nn.Linear(8, 6)
-        self.linear2 = torch.nn.Linear(6, 4)
-        self.linear3 = torch.nn.Linear(4, 1)
-        self.active = torch.nn.Softsign()
-        self.sigmoid = torch.nn.Sigmoid()
-
-    def forward(self, x):
-        x = self.active(self.linear1(x))
-        x = self.active(self.linear2(x))
-        x = self.sigmoid(self.linear3(x))
-        return x
-
-model = MYMODEL()
-criteration = torch.nn.BCELoss(reduction='sum')
-optimition = torch.optim.SGD(model.parameters(), lr=0.01)
-
-for epoch in range(10000):
-    y_prey = model(x_data)
-    loss = criteration(y_prey, y_data)
-    if (epoch % 100) == 0:
-        print(epoch, loss.item())
-    optimition.zero_grad()
-    loss.backward()
-    optimition.step()
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
     
-x_test = torch.from_numpy(row_data[-7:, :-1])
-y_test = model(x_test)
+    def __len__(self):
+        return self.len
 
-print('y_test:', y_test)
+class LinearModule(torch.nn.Module):
+    def __init__(self):
+        super(LinearModule, self).__init__()
+        self.linear = torch.nn.Linear(8, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+        
+    def forward(self, x):
+        y_prey = self.sigmoid(self.linear(x))
+        return y_prey
+    
+if __name__ == '__main__':
+    datasets = DiateatsDatasets('/datasets/diabetes/diabetes.csv.gz')
+    
+
