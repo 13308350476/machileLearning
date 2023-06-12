3772db7ac6c4375746abb783442c3fc073f6e02a
13308350476 <zxc723067485811@qq.com>
2023-06-12 17:46:24 +0800

    20230612

--- LINEMODEL/train.py
+++ LINEMODEL/train.py
@@ -17,13 +17,13 @@ train_loader = DataLoader(dataset=train_sets, shuffle=True, batch_size=batch_siz
 class CNN(torch.nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = torch.nn.Conv2d(1, 5, kernel_size=3)
-        self.conv2 = torch.nn.Conv2d(5, 10, kernel_size=4)
-        self.conv3 = torch.nn.Conv2d(10, 20, kernel_size=2)
+        self.conv1 = torch.nn.Conv2d(1, 10, 3)
+        self.conv2 = torch.nn.Conv2d(10, 20, 3)
+        self.conv3 = torch.nn.Conv2d(20, 30, 3)
         self.pooling = torch.nn.MaxPool2d(2)
-        self.linear1 = torch.nn.Linear(80, 60)
-        self.linear2 = torch.nn.Linear(60, 40)
-        self.linear3 = torch.nn.Linear(40, 10)
+        self.l1 = torch.nn.Linear(30, 20)
+        self.l2 = torch.nn.Linear(20, 10)
+        
 
     def forward(self, x):
         batchsize = x.size(0)
@@ -31,29 +31,32 @@ class CNN(torch.nn.Module):
         x = F.relu(self.pooling(self.conv2(x)))
         x = F.relu(self.pooling(self.conv3(x)))
         x = x.view(batchsize, -1)
-        x = F.relu(self.linear1(x))
-        x = F.relu(self.linear2(x))
-        x = self.linear3(x)
+        # print(x.shape)
+        x = F.relu(self.l1(x))
+        x = self.l2(x)
         return x
     
 modle = CNN()
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'GPU')
+modle.to(device)
+
 criteration = torch.nn.CrossEntropyLoss()
 optimition = torch.optim.SGD(modle.parameters(), lr=0.05, momentum=0.5)
 
 def train(epoch):
-    total_loss = 0
+    toltal_loss = 0
     for index, data in enumerate(train_loader, 0):
         x_data, y_data = data
+        x_data, y_data = x_data.to(device), y_data.to(device)
         y_prey = modle(x_data)
         loss = criteration(y_prey, y_data)
-        total_loss += loss
+        toltal_loss += loss
         optimition.zero_grad()
         loss.backward()
         optimition.step()
         if index % 300 == 299:
-            print('[%d, %5d] loss: %.3f' % (epoch + 1, index + 1, total_loss / 300))
-            total_loss = 0.0
+            print('[%d, %d] loss: %.3f' % (epoch + 1, index + 1, toltal_loss / 300))
+            toltal_loss = 0.0
 
-if __name__ == '__main__':
-    for epoch in range(1):
-        train(epoch)
+for epoch in range(1):
+    train(epoch)
