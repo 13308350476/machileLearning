7a94477bd23fb5db91fabaf3636ede5ec51ed0a2
13308350476 <61304069+13308350476@users.noreply.github.com>
2023-06-19 08:25:44 +0800

    RNN classifier
    RNN classifier

--- LINEMODEL/RNNClassfier.py
+++ LINEMODEL/RNNClassfier.py
@@ -1,7 +1,43 @@
 import torch
 from torch.utils.data import Dataset
+import gzip
+import csv
 
-class DataSet(Dataset):
+class NameDataSet(Dataset):
     def __init__(self, is_trainset=True):
         file_name = '../datasets/name/names_train.csv.gz' if is_trainset else '../datasets/name/names_test.csv.gz'
+        with gzip.open(file_name, 'rt') as f:
+            reader = csv.reader(f)
+            rows = list(reader)
+        self.names = [rows[0] for row in rows]
+        self.len = len(self.names)
+        self.contries = [rows[1] for row in rows]
+        self.country_list = list(sorted(set(self.contries)))
+        self.country_dict = self.getCountryDict()
+        self.contry_num = len(self.country_list)
+    
+    def __getitem__(self, index):
+        return self.names[index], self.country_dict[self.contries[index]]
+    
+    def __len__(self):
+        return self.len
+    
+    def getCountryDict(self):
+        conunry_dict = dict()
+        for idx, countryname in enumerate(self.country_list, 0):
+            conunry_dict[countryname] = idx
+        return conunry_dict
+    
+    def id2xcountry(self, index):
+        return self.country_list(index)
+    
+    def getCountriesNum(self):
+        return self.contry_num
+
+
+        
+    
+class NET(torch.nn.Module):
+    def __init__(self):
+        super(NET, self).__init__()
         
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from typing import Any
-import torch
+# import torch
 import math
+import gzip
+import csv
 
 def reverse(input):
     compose = input.split(' ')
@@ -27,19 +29,8 @@ if __name__ == "__main__":
     rw = reverse(input)
     print(rw)
     '''
-    a = 1
-    tup = (a,2,3,4)
-    a = 5
-    print(type(tup))
-
-    x = torch.linspace(-math.pi, math.pi, 4)
-    print(x.shape)
-    print(x)
-    p = torch.tensor([1, 2, 3])
-    print(p.shape)
-    print(p)
-    xx = x.unsqueeze(-1).pow(p)
-    print(xx.shape)
-    print(xx)
-
-    print(0.2 % 1000)
+    file_name = './datasets/name/names_train.csv.gz'
+    with gzip.open(file_name, 'rt') as f:
+        reader = csv.reader(f)
+        rows = list(reader)
+    print(rows)
